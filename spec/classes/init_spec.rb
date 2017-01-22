51430ab1642231198cbc3f7544f457ead222cd99
Evgeny Piven <thepegasd@gmail.com>
2017-01-22 20:03:41 +0300

    Unit testing for file contents and correct/incorrect time specification

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -3,5 +3,21 @@ require 'spec_helper'
 describe 'cron' do
   context 'with default values for all parameters' do
     it { should contain_class('cron') }
+    it { should contain_file('/etc/cron.d').with({
+      :ensure  => 'directory',
+      :owner   => 'root',
+      :group   => 'root',
+      :mode    => '0755',
+      :recurse => true,
+      :purge   => true,
+    }) }
+  end
+
+  context 'with custom dir permissions' do
+    let (:params) { { :dir_mode => '0750' } }
+
+    it { should contain_file('/etc/cron.d').with({
+      :mode => '0750'
+    }) }
   end
 end
--- spec/defines/cron-job_spec.rb
+++ spec/defines/cron-job_spec.rb
@@ -5,14 +5,35 @@ describe 'cron::job' do
 
   context 'job with default values' do
     let(:params) { {
-      :command => '/usr/bin/backup'
+      :command => '/usr/bin/backup',
     } }
 
+    it { should compile }
     it { should contain_file('cron-job_backup').with({
       :ensure => 'file',
       :owner  => 'root',
       :group  => 'root',
       :mode   => '0644',
-    }) }
+    }).with_content(/^* * * * * root \/usr\/bin\/backup$/) }
+  end
+
+  context 'with custom launch time' do
+    let(:params) { {
+      :command => '/usr/bin/backup',
+      :minute  => [50, '20'],
+      :hour    => [1, 5],
+      :weekday => 3,
+    } }
+
+    it { should compile }
+  end
+
+  context 'with incorrect launch time' do
+    let(:params) { {
+      :command => '/usr/bin/backup',
+      :minute  => 66,
+    } }
+
+    it { should_not compile }
   end
 end
