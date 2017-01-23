17b62917e51ba02c882adb302c655010543e02ea
Evgeny Piven <thepegasd@gmail.com>
2017-01-24 00:43:35 +0300

    Incron tests

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,8 +1,8 @@
 require 'spec_helper'
 
 describe 'cron' do
+
   context 'with default values for all parameters' do
-    it { should contain_class('cron') }
     it { should contain_file('/etc/cron.d').with({
       :ensure  => 'directory',
       :owner   => 'root',
@@ -11,13 +11,46 @@ describe 'cron' do
       :recurse => true,
       :purge   => true,
     }) }
+
+    it { should contain_file('/etc/incron.d').with({
+      :ensure => 'absent',
+      :force  => true,
+    }) }
   end
 
   context 'with custom dir permissions' do
-    let (:params) { { :dir_mode => '0750' } }
+    let (:params) { {
+      :dir_mode   => '0750',
+      :use_incron => true,
+    } }
 
-    it { should contain_file('/etc/cron.d').with({
-      :mode => '0750'
+    it { should contain_file('/etc/cron.d').with({ :mode => '0750' }) }
+    it { should contain_file('/etc/incron.d').with({ :mode => '0750' }) }
+  end
+
+  context 'with incron enabled' do
+    let (:params) { { :use_incron => true } }
+
+    it { should contain_file('/etc/incron.d').with({
+      :ensure  => 'directory',
+      :owner   => 'root',
+      :group   => 'root',
+      :mode    => '0755',
+      :recurse => true,
+      :purge   => true,
+    }) }
+
+    it { should contain_service('incron').with({
+      :ensure     => 'running',
+      :enable     => true,
+      :hasrestart => true,
+      :hasstatus  => true,
+      :require    => "Package['incron']",
+    }) }
+
+    it { should contain_package('incron').with({
+      :ensure => 'present',
     }) }
   end
+
 end
