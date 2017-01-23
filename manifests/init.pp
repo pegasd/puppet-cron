dfd59606b34c4c97a6bc41a366cd8956415b240a
Evgeny Piven <thepegasd@gmail.com>
2017-01-24 01:18:44 +0300

    incron support

--- manifests/init.pp
+++ manifests/init.pp
@@ -8,8 +8,11 @@
 #   include cron
 #
 # @param dir_mode /etc/cron.d directory mode
+# @param use_incron Whether to also use incron.
+#   Note: When this is off (by default!), the module will keep /etc/incron.d directory clean!
 class cron (
-  String[4, 4] $dir_mode = '0755',
+  String[4, 4] $dir_mode   = '0755',
+  Boolean      $use_incron = false,
 ) {
 
   file { '/etc/cron.d':
@@ -20,6 +23,39 @@ class cron (
     owner   => 'root',
     group   => 'root',
     mode    => $dir_mode,
+  }
+
+  if $use_incron {
+    file { '/etc/incron.d':
+      ensure  => directory,
+      recurse => true,
+      purge   => true,
+      force   => true,
+      owner   => 'root',
+      group   => 'root',
+      mode    => $dir_mode,
+    }
+
+    package { 'incron':
+      ensure => present,
+    }
+
+    service { 'incron':
+      ensure     => running,
+      enable     => true,
+      hasrestart => true,
+      hasstatus  => true,
+      require    => Package['incron'],
+    }
+  } else {
+    file { '/etc/incron.d':
+      ensure => absent,
+      force  => true,
+    }
+
+    package { 'incron':
+      ensure => absent,
+    }
   }
 
 }
