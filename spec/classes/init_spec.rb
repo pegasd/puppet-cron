ad6832a880a1221d0362b989bf7e010a88f48ecd
Eugene Piven <thepegasd@gmail.com>
2017-02-09 10:17:08 +0300

    Removing incron: changing tests

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -4,56 +4,51 @@ describe 'cron' do
 
   context 'with default values for all parameters' do
     it { is_expected.to compile.with_all_deps }
+
     it { is_expected.to contain_class('cron') }
+
+    it { is_expected.to contain_class('cron::install') }
+    it { is_expected.to contain_class('cron::config') }
+    it { is_expected.to contain_class('cron::service') }
+
+    describe 'cron::install' do
+      it { is_expected.to compile.with_all_deps }
+      it { is_expected.to contain_package('cron').with_ensure(:present) }
+    end
+
+    describe 'cron::config' do
+      it { is_expected.to contain_file('/etc/cron.d').with(
+        :ensure  => :directory,
+        :owner   => 'root',
+        :group   => 'root',
+        :mode    => '0755',
+        :recurse => true,
+        :purge   => true,
+      ) }
+    end
+  end
+
+  context 'with custom dir_mode' do
+    let(:params) { { :dir_mode => '0700' } }
+
+    it { is_expected.to compile.with_all_deps }
+    it { is_expected.to contain_file('/etc/cron.d').with(:mode => '0700') }
+  end
+
+  context 'with ensure => absent' do
+    let(:params) { { :ensure => :absent } }
+
+    it { is_expected.to compile.with_all_deps }
+
+    it { is_expected.not_to contain_class('cron::install') }
+    it { is_expected.not_to contain_class('cron::config') }
+    it { is_expected.not_to contain_class('cron::service') }
+
     it { is_expected.to contain_file('/etc/cron.d').with(
-      :ensure  => :directory,
-      :owner   => 'root',
-      :group   => 'root',
-      :mode    => '0755',
-      :recurse => true,
-      :purge   => true,
-    ) }
-    it { is_expected.to contain_file('/etc/incron.d').with(
-      :ensure => :absent,
+      :ensure => absent,
       :force  => true,
     ) }
-    it { is_expected.to contain_package('incron').with(:ensure => :absent) }
-  end
-
-  context 'with custom dir permissions' do
-    let(:params) { {
-      :crond_mode   => '0750',
-      :incrond_mode => '0750',
-      :use_incron   => true,
-    } }
-
-    it { is_expected.to compile.with_all_deps }
-    it { is_expected.to contain_file('/etc/cron.d').with(:mode => '0750') }
-    it { is_expected.to contain_file('/etc/incron.d').with(:mode => '0750') }
-  end
-
-  context 'with incron enabled' do
-    let(:params) { { :use_incron => true } }
-
-    it { is_expected.to compile.with_all_deps }
-    it { is_expected.to contain_file('/etc/incron.d').with(
-      :ensure  => :directory,
-      :owner   => 'root',
-      :group   => 'root',
-      :mode    => '0755',
-      :recurse => true,
-      :purge   => true,
-    ) }
-
-    it { is_expected.to contain_service('incron').with(
-      :ensure     => :running,
-      :enable     => true,
-      :hasrestart => true,
-      :hasstatus  => true,
-      :require    => 'Package[incron]'
-    ) }
-
-    it { is_expected.to contain_package('incron').with(:ensure => :present) }
+    it { is_expected.to contain_package('cron').with_ensure(:absent) }
   end
 
 end
