ef0c62545f36f8cba891ce34e60446ce8ca6f82c
Eugene Piven <thepegasd@gmail.com>
2017-02-10 12:21:30 +0300

    Full resource coverage, refactoring

--- spec/classes/cron_spec.rb
+++ spec/classes/cron_spec.rb
@@ -18,27 +18,36 @@ describe 'cron' do
 
     describe 'cron::config' do
       it { is_expected.to contain_file('/etc/cron.d').with(
-        :ensure  => :directory,
-        :owner   => 'root',
-        :group   => 'root',
-        :mode    => '0755',
-        :recurse => true,
-        :purge   => true,
+        ensure:  :directory,
+        owner:   'root',
+        group:   'root',
+        mode:    '0755',
+        recurse: true,
+        purge:   true,
+      ) }
+    end
+
+    describe 'cron::service' do
+      it { is_expected.to contain_service('cron').with(
+        ensure: :running,
+        enable: true,
       ) }
     end
   end
 
   context 'with custom dir_mode' do
-    let(:params) { { :dir_mode => '0700' } }
+    let(:params) { { dir_mode: '0700' } }
 
     it { is_expected.to compile.with_all_deps }
-    it { is_expected.to contain_file('/etc/cron.d').with(:mode => '0700') }
+    it { is_expected.to contain_file('/etc/cron.d').with(mode: '0700') }
   end
 
   context 'with ensure => absent' do
-    let(:params) { { :ensure => 'absent' } }
+    let(:params) { { ensure: 'absent' } }
 
     it { is_expected.to compile.with_all_deps }
+
+    it { is_expected.to contain_class('cron::remove') }
 
     it { is_expected.not_to contain_class('cron::install') }
     it { is_expected.not_to contain_class('cron::config') }
