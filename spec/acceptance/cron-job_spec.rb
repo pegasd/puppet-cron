cc0c0276e54a1a2a4458fc2ad81c8b818f29fd36
Eugene Piven <thepegasd@gmail.com>
2017-02-10 12:22:24 +0300

    Fixing acceptance tests after removing incron resources

--- spec/acceptance/cron-job_spec.rb
+++ spec/acceptance/cron-job_spec.rb
@@ -5,7 +5,7 @@ describe 'tidy cron::job' do
   describe 'first run with two cron::job resources' do
     pp = <<-EOS
     
-class { 'cron': crond_mode => '0750' }
+class { 'cron': dir_mode => '0750' }
 
 cron::job {
   'backup':
