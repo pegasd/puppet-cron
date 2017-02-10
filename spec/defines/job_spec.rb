6153e8db971b8b3d5d499cf4214626369e050138
Eugene Piven <thepegasd@gmail.com>
2017-02-10 12:21:48 +0300

    Refactoring hashes

--- spec/defines/job_spec.rb
+++ spec/defines/job_spec.rb
@@ -5,33 +5,35 @@ describe 'cron::job' do
 
   context 'job with default values' do
     let(:params) { {
-      :command => '/usr/bin/backup',
+      command: '/usr/bin/backup',
     } }
 
     it { is_expected.to compile.with_all_deps }
     it { is_expected.to contain_file('/etc/cron.d/backup').with(
-      :ensure => :file,
-      :owner  => 'root',
-      :group  => 'root',
-      :mode   => '0644',
-    ).with_content(/^\* \* \* \* \* root \/usr\/bin\/backup$/) }
+      ensure:  :file,
+      owner:   'root',
+      group:   'root',
+      mode:    '0644',
+      content: /^\* \* \* \* \* root \/usr\/bin\/backup$/)
+    }
   end
 
   context 'with custom launch time' do
     let(:params) { {
-      :command => '/usr/bin/backup',
-      :minute  => [50, '20'],
-      :hour    => [1, 5],
-      :weekday => '*/2',
+      command: '/usr/bin/backup',
+      minute:  [50, '20'],
+      hour:    [1, 5],
+      weekday: '*/2',
     } }
 
     it { is_expected.to compile.with_all_deps }
     it { is_expected.to contain_file('/etc/cron.d/backup').with(
-      :ensure => :file,
-      :owner  => 'root',
-      :group  => 'root',
-      :mode   => '0644',
-    ).with_content(/^50,20 1,5 \* \* \*\/2 root \/usr\/bin\/backup$/) }
+      ensure:  :file,
+      owner:   'root',
+      group:   'root',
+      mode:    '0644',
+      content: /^50,20 1,5 \* \* \*\/2 root \/usr\/bin\/backup$/
+    ) }
   end
 
   context 'with minute => 66' do
