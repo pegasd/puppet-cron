fae15187dc6f9ee7bda298bcb4fe1e70199c9880
Eugene Piven <thepegasd@gmail.com>
2017-02-15 23:36:16 +0300

    cron::cron2string documentation

--- functions/cron2string.pp
+++ functions/cron2string.pp
@@ -1,3 +1,10 @@
+# cron::cron2string function
+#
+# This function converts any cron timing value to a string similar to these examples:
+# '*', '5', '17,34', '*/2', '2-59/10', '
+#
+# @param cron_value A variant of any of cron's internal timing structures (minute, hour, monthday, month, weekday)
+# @return [String] Representation of the cron time in a proper string format ready to be plugged into a template
 function cron::cron2string (
   Variant[
     Cron::Minute,
@@ -5,7 +12,7 @@ function cron::cron2string (
     Cron::Monthday,
     Cron::Month,
     Cron::Weekday
-  ] $cron_value = '',
+  ] $cron_value = '*',
 ) {
 
   # Can we please get a profiler here?
