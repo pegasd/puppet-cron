0630cf50e20dae6156901a67f81f8faa13cfe217
Eugene Piven <thepegasd@gmail.com>
2017-02-28 01:14:11 +0300

    cron::prep4cron function

---
+++ functions/prep4cron.pp
@@ -0,0 +1,29 @@
+# cron::prep4cron function
+#
+# This functions prepares any cron::job custom timing value to be
+# used as Puppet internal cron's resource argument
+#
+# @param cron_value A variant of any of cron's internal timing structures (minute, hour, monthday, month, weekday)
+# @return [String, Integer, Array[Integer]] Representation of the cron time value in a proper format suited for
+#   internal Puppet's cron resource.
+function cron::prep4cron (
+  Variant[
+    Cron::Minute,
+    Cron::Hour,
+    Cron::Monthday,
+    Cron::Month,
+    Cron::Weekday
+  ] $cron_value = '*',
+) {
+
+  if $cron_value.is_a(Array) {
+
+    sort(unique($cron_value))
+
+  } else {
+
+    $cron_value
+
+  }
+
+}
