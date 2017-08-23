061b86247211ac67c85f97340ccaca59880b203e
Eugene Piven <thepegasd@gmail.com>
2017-08-23 11:06:25 +0300

    Maybe this'll work

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -4,29 +4,20 @@ require 'sem_version'
 
 ENV['PUPPET_INSTALL_TYPE'] = 'agent' if ENV['PUPPET_INSTALL_TYPE'].nil?
 
-# Otherwise puppet defaults to /etc/puppetlabs/code
-configure_defaults_on hosts, 'foss' unless ENV['PUPPET_INSTALL_TYPE'] == 'agent'
-
 if ENV['PUPPET_INSTALL_TYPE'] == 'gem'
-  install_puppet_from_gem_on(
-    hosts,
-    :version => (
-    ENV['PUPPET_INSTALL_VERSION'] || ENV['PUPPET_VERSION'] || '~> 4.0'
-    )
-  )
+  puppet_version = ENV['PUPPET_VERSION'] || '~> 4.0'
+  install_puppet_from_gem_on(hosts, version: puppet_version)
 else
   run_puppet_install_helper unless ENV['BEAKER_provision'] == 'no'
 end
 
 RSpec.configure do |c|
-  proj_root = File.expand_path(File.join(File.dirname(__FILE__), '..'))
-
   c.color     = true
   c.formatter = :documentation
 
   c.before :suite do
     hosts.each do |host|
-      copy_module_to(host, :source => proj_root, :module_name => 'cron')
+      copy_root_module_to(host, module_name: 'cron', target_module_path: '/etc/puppetlabs/code/modules')
       on host, puppet('module', 'install', 'puppetlabs-stdlib')
     end
   end
