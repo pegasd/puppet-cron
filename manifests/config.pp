b232ef2d20ab29db3e79c7eb4289a64fa8841701
Eugene Piven <thepegasd@gmail.com>
2017-12-21 21:18:01 +0300

    Fail when both allowed and denied users were specified

--- manifests/config.pp
+++ manifests/config.pp
@@ -3,6 +3,10 @@
 # Various cron configuration files
 class cron::config {
 
+  if !empty($::cron::allowed_users) and !empty($::cron::denied_users) {
+    fail('Either allowed or denied cron users must be specified, not both.')
+  }
+
   file { '/etc/cron.deny':
     ensure => absent,
     force  => true,
