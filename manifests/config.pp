bc7896d12fad92d6cda9108dec3e7169e51b1cc7
Eugene Piven <thepegasd@gmail.com>
2017-12-21 21:28:55 +0300

    cron.allow and cron.deny implementation logic

--- manifests/config.pp
+++ manifests/config.pp
@@ -7,13 +7,29 @@ class cron::config {
     fail('Either allowed or denied cron users must be specified, not both.')
   }
 
+  if !empty($::cron::allowed_users) {
+    $ensure_allow = file
+    $ensure_deny = absent
+  } elsif !empty($::cron::denied_users) {
+    $ensure_allow = absent
+    $ensure_deny = file
+  } else {
+    $ensure_allow = absent
+    $ensure_deny = absent
+  }
+
   file { '/etc/cron.deny':
-    ensure => absent,
-    force  => true,
+    ensure  => $ensure_deny,
+    force   => true,
+    content => '',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
   }
 
   file { '/etc/cron.allow':
-    ensure  => file,
+    ensure  => $ensure_allow,
+    force   => true,
     content => '',
     owner   => 'root',
     group   => 'root',
