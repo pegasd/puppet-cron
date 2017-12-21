0b9b758ed7b5d4ef179a9096394b1950260bfeb2
Eugene Piven <thepegasd@gmail.com>
2017-12-21 22:10:57 +0300

    Updated documentation

--- manifests/init.pp
+++ manifests/init.pp
@@ -7,6 +7,7 @@
 # @author Eugene Piven <epiven@gmail.com>
 #
 # @see https://github.com/pegasd/puppet-cron
+# @see manpages crontab(1), crontab(5), cron(8)
 #
 # @example Declaring the cron class
 #   include cron
@@ -17,9 +18,15 @@
 # @example Removing all cron-related resources from the system
 #   class { 'cron': ensure => absent }
 #
+# @example Deny crontab(1) usage to all users except root
+#   class { 'cron': allowed_users => [ 'root' ] }
+#
 # @param ensure Whether to enable or disable cron on the system.
 # @param purge_crond Also purge unmanaged files in /etc/cron.d directory
 # @param purge_noop Run purging in `noop` mode.
+# @param allowed_users List of users that are specifically allowed to use `crontab(1)`.
+#   If none are specified, all users are allowed.
+# @param denied_users List of users that are specifically denied to use `crontab(1)`.
 class cron (
   Enum[present, absent] $ensure        = present,
   Boolean               $purge_crond   = false,
