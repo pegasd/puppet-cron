46f942ae7d0d2b967db1396429249bdd343fab06
Eugene Piven <thepegasd@gmail.com>
2018-02-25 13:19:01 +0300

    Test on Ubuntu 18.04

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -4,14 +4,8 @@ require 'beaker-rspec'
 require 'beaker/puppet_install_helper'
 require 'sem_version'
 
-ENV['PUPPET_INSTALL_TYPE'] = 'agent' if ENV['PUPPET_INSTALL_TYPE'].nil?
-
-if ENV['PUPPET_INSTALL_TYPE'] == 'gem'
-  puppet_version = ENV.fetch('PUPPET_VERSION', '~> 5.0')
-  install_puppet_from_gem_on(hosts, version: puppet_version)
-else
-  run_puppet_install_helper unless ENV['BEAKER_provision'] == 'no'
-end
+puppet_version = ENV.fetch('PUPPET_VERSION', '~> 5.0')
+install_puppet_from_gem_on(hosts, version: puppet_version)
 
 RSpec.configure do |c|
   c.color     = true
