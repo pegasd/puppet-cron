16e82c377936c2a8882545302ee1d2df5424bf95
Eugene Piven <thepegasd@gmail.com>
2018-03-10 02:11:49 +0300

    Acceptance tests for `/etc/cron.allow`

---
+++ spec/acceptance/regular_user_crontab_spec.rb
@@ -0,0 +1,51 @@
+# frozen_string_literal: true
+
+require 'spec_helper_acceptance'
+
+describe 'crontab(1)' do
+  context 'luke is not ready yet' do
+    pp = <<~PUPPET
+
+      include cron
+
+      user { 'luke': ensure => present }
+
+    PUPPET
+
+    apply_and_test_idempotence pp
+
+    describe command('sudo -u luke env EDITOR=cat crontab -e') do
+      its(:exit_status) { is_expected.to eq 1 }
+      its(:stderr) { is_expected.to match(/luke.*not allowed to use.*crontab/) }
+    end
+  end
+
+  context 'luke has force' do
+    pp = <<~PUPPET
+
+      class { 'cron':
+        allowed_users => [ 'luke' ],
+      }
+
+      user { 'luke': ensure => present }
+
+    PUPPET
+
+    apply_and_test_idempotence pp
+
+    describe command('sudo -u luke env EDITOR=cat crontab -e') do
+      its(:exit_status) { is_expected.to eq 0 }
+    end
+  end
+
+  context 'clean up' do
+    pp = <<~PUPPET
+      include cron
+      user { 'luke': ensure => absent }
+    PUPPET
+
+    apply_and_test_idempotence pp
+
+    describe user('luke') { it { is_expected.not_to exist } }
+  end
+end
