ca48ac1309da663d73fc5a686c390a97cb2258fe
Eugene Piven <thepegasd@gmail.com>
2024-07-30 12:39:40 +0300

    Trying out random stuff hoping it will work :: 3

--- spec/acceptance/purge_cron_spec.rb
+++ spec/acceptance/purge_cron_spec.rb
@@ -2,58 +2,56 @@
 
 require 'spec_helper_acceptance'
 
-describe 'cron::purge' do
-  describe 'first run with two cron::job resources' do
-    let(:pp) do
-      <<~PUPPET
+describe 'first run with two cron::job resources' do
+  let(:pp) do
+    <<~PUPPET
 
-        include cron
+      include cron
 
-        cron::job { 'backup':
-          command => '/usr/bin/backup';
-        }
+      cron::job { 'backup':
+        command => '/usr/bin/backup';
+      }
 
-        cron::job { 'other-backup':
-          command => '/usr/bin/other-backup';
-        }
+      cron::job { 'other-backup':
+        command => '/usr/bin/other-backup';
+      }
 
-      PUPPET
-    end
-
-    idempotent_apply(pp)
-
-    describe cron do
-      it { is_expected.to have_entry '* * * * * /usr/bin/backup' }
-      it { is_expected.to have_entry '* * * * * /usr/bin/other-backup' }
-    end
-
-    describe service('cron') do
-      it { is_expected.to be_running }
-    end
-
-    describe package('cron') do
-      it { is_expected.to be_installed }
-    end
+    PUPPET
   end
 
-  describe 'second run with one of the resources removed' do
-    let(:pp) do
-      <<~PUPPET
+  idempotent_apply(pp)
 
-        include cron
+  describe cron do
+    it { is_expected.to have_entry '* * * * * /usr/bin/backup' }
+    it { is_expected.to have_entry '* * * * * /usr/bin/other-backup' }
+  end
 
-        cron::job { 'backup':
-          command => '/usr/bin/backup';
-        }
+  describe service('cron') do
+    it { is_expected.to be_running }
+  end
 
-      PUPPET
-    end
+  describe package('cron') do
+    it { is_expected.to be_installed }
+  end
+end
 
-    idempotent_apply(pp)
+describe 'second run with one of the resources removed' do
+  let(:pp) do
+    <<~PUPPET
 
-    describe cron do
-      it { is_expected.to have_entry '* * * * * /usr/bin/backup' }
-      it { is_expected.not_to have_entry '* * * * * /usr/bin/other-backup' }
-    end
+      include cron
+
+      cron::job { 'backup':
+        command => '/usr/bin/backup';
+      }
+
+    PUPPET
+  end
+
+  idempotent_apply(pp)
+
+  describe cron do
+    it { is_expected.to have_entry '* * * * * /usr/bin/backup' }
+    it { is_expected.not_to have_entry '* * * * * /usr/bin/other-backup' }
   end
 end
