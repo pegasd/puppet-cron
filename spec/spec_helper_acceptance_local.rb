0823f86da3aaa3dd00b35e053a88460ef22ae7a8
Eugene Piven <thepegasd@gmail.com>
2024-07-30 13:28:40 +0300

    Fix local spec acceptance

--- spec/spec_helper_acceptance_local.rb
+++ spec/spec_helper_acceptance_local.rb
@@ -1,3 +1,12 @@
+# frozen_string_literal: true
+
+require 'singleton'
+
+class LitmusHelper
+  include Singleton
+  include PuppetLitmus
+end
+
 RSpec.configure do |c|
   c.formatter = :documentation
   c.color     = true
--- spec/spec_helper_local.rb
+++ spec/spec_helper_local.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 RSpec.configure do |c|
   c.formatter = :documentation
   c.color     = true
