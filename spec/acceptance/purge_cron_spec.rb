672e7d7563763775ed89cf20bfc7297e00405023
Eugene Piven <thepegasd@gmail.com>
2024-07-30 12:46:39 +0300

    Trying out random stuff hoping it will work :: 4

--- spec/acceptance/purge_cron_spec.rb
+++ spec/acceptance/purge_cron_spec.rb
@@ -2,56 +2,55 @@
 
 require 'spec_helper_acceptance'
 
-describe 'first run with two cron::job resources' do
-  let(:pp) do
-    <<~PUPPET
+describe 'cron::purge' do
+  describe 'first run with two cron::job resources' do
+    let(:pp) do
+      <<-MANIFEST
+        include cron
+        cron::job { 'backup':
+          command => '/usr/bin/backup';
+        }
+        cron::job { 'other-backup':
+          command => '/usr/bin/other-backup';
+        }
+      MANIFEST
+    end
 
-      include cron
+    it 'behaves idempotently' do
+      idempotent_apply(pp)
+    end
 
-      cron::job { 'backup':
-        command => '/usr/bin/backup';
-      }
+    describe cron do
+      it { is_expected.to have_entry '* * * * * /usr/bin/backup' }
+      it { is_expected.to have_entry '* * * * * /usr/bin/other-backup' }
+    end
 
-      cron::job { 'other-backup':
-        command => '/usr/bin/other-backup';
-      }
+    describe service('cron') do
+      it { is_expected.to be_running }
+    end
 
-    PUPPET
+    describe package('cron') do
+      it { is_expected.to be_installed }
+    end
   end
 
-  idempotent_apply(pp)
+  describe 'second run with one of the resources removed' do
+    let(:pp) do
+      <<-MANIFEST
+        include cron
+        cron::job { 'backup':
+          command => '/usr/bin/backup';
+        }
+      MANIFEST
+    end
 
-  describe cron do
-    it { is_expected.to have_entry '* * * * * /usr/bin/backup' }
-    it { is_expected.to have_entry '* * * * * /usr/bin/other-backup' }
-  end
+    it 'behaves idempotently' do
+      idempotent_apply(pp)
+    end
 
-  describe service('cron') do
-    it { is_expected.to be_running }
-  end
-
-  describe package('cron') do
-    it { is_expected.to be_installed }
-  end
-end
-
-describe 'second run with one of the resources removed' do
-  let(:pp) do
-    <<~PUPPET
-
-      include cron
-
-      cron::job { 'backup':
-        command => '/usr/bin/backup';
-      }
-
-    PUPPET
-  end
-
-  idempotent_apply(pp)
-
-  describe cron do
-    it { is_expected.to have_entry '* * * * * /usr/bin/backup' }
-    it { is_expected.not_to have_entry '* * * * * /usr/bin/other-backup' }
+    describe cron do
+      it { is_expected.to have_entry '* * * * * /usr/bin/backup' }
+      it { is_expected.not_to have_entry '* * * * * /usr/bin/other-backup' }
+    end
   end
 end
